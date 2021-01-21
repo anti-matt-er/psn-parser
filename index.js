557d82ab1902106daf30016872b9d514897eeb89
anti-matt-er <m.burrows@live.co.uk>
2021-01-21 16:51:05 +0000

    implement bet extraction

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 'use strict';
 
 class PSN {
+    big_blind = 0;
+    small_blind = 0;
+    ante = 0;
+    btn_notation = false;
+    dealer = false;
+    seats = 0;
+    max_seats = 0;
+
     constructor(notation) {
         this.notation = PSN.normalise_notation(notation);
         this.extract();
@@ -22,5 +30,33 @@ class PSN {
         return clean;
     }
 
+    extract() {
+        this.extract_game();
+    }
+
+    extract_game() {
+        this.extract_bets();
+        //this.extract_seats();
+    }
+
+    extract_bets() {
+        let notation_pieces = this.notation.split(' ', 4);
+        let bets = notation_pieces[0].split('|');
+        if (bets.length === 3) {
+            this.ante = parseInt(bets[0]);
+            this.small_blind = parseInt(bets[1]);
+            this.big_blind = parseInt(bets[2]);
+        }
+        if (bets.length === 2) {
+            this.ante = 0;
+            this.small_blind = parseInt(bets[0]);
+            this.big_blind = parseInt(bets[1]);
+        }
+        if (bets.length === 1) {
+            this.ante = 0;
+            this.big_blind = parseInt(bets[0]);
+            this.small_blind = parseInt(Math.ceil(this.big_blind / 2));
+        }
+    }
 }
 exports.PSN = PSN;
