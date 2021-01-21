9d4386dc5dc529313f9c168517d3b4295a60f900
anti-matt-er <m.burrows@live.co.uk>
2021-01-21 16:50:47 +0000

    implement whitespace normalisation

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 'use strict';
 
 class PSN {
+    constructor(notation) {
+        this.notation = PSN.normalise_notation(notation);
+        this.extract();
+    }
+
+    static normalise_notation(notation) {
+        let normalised = PSN.clean_whitespace(notation);
+        if (normalised.slice(0, 4) !== 'NLH ') {
+            throw 'Error: Invalid syntax!';
+        }
+        return normalised.slice(4);
+    }
+
+    static clean_whitespace(notation) {
+        let clean = notation;
+        clean = clean.replace(/\s/g, ' ');
+        clean = clean.replace(/\s\s+/g, ' ');
+        clean = clean.trimRight();
+        return clean;
+    }
 
 }
 exports.PSN = PSN;
